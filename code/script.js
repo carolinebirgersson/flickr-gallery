a62281118e4d7475fbbb3b4658ab72fcbb166591
caroline birgersson <caroline@birgerochblom.se>
2021-06-04 10:42:39 +0200

    including tags

--- code/script.js
+++ code/script.js
@@ -1,20 +1,24 @@
-const container = document.getElementById("gallery");
 
-fetch(
-  `https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=449422936ab75f7bef9649f4cff24200&format=json&nojsoncallback=1`
-)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    data.photos.photo.map((gallery) => {
-      const id = gallery.id;
-      const serverId = gallery.server;
-      const secret = gallery.secret;
-      container.innerHTML += `<article><img src="https://live.staticflickr.com/${serverId}/${id}_${secret}_w.jpg"/></article>`;
+const galleryFunction = () => {
+  const container = document.getElementById("gallery");
+  
+  fetch(
+    `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=449422936ab75f7bef9649f4cff24200&tags=catsl&format=json&nojsoncallback=1`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      data.photos.photo.map((gallery) => {
+        const id = gallery.id;
+        const serverId = gallery.server;
+        const secret = gallery.secret;
+        container.innerHTML += `<img src="https://live.staticflickr.com/${serverId}/${id}_${secret}_w.jpg"/>`;
+      });
+    })
+    .catch((err) => {
+      console.log("caught error", err);
     });
-  })
-  .catch((err) => {
-    console.log("caught error", err);
-  });
+  }
+
+  galleryFunction();
