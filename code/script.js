5401df7eec6f58cd5a4e081cf69319a6728fab9b
caroline birgersson <caroline@birgerochblom.se>
2021-06-06 17:13:02 +0200

    changing the loader + adding an accordion

--- code/script.js
+++ code/script.js
@@ -1,21 +1,27 @@
+function toggle () {
+  this.classList.toggle("open")
+}
+
+document.getElementById("accordion").onclick = toggle
+
 const API_KEY = "449422936ab75f7bef9649f4cff24200";
-const API_URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=olafureliasson&format=json&nojsoncallback=1`
+const API_URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=lecorbusier&format=json&nojsoncallback=1`
 
-const loader = document.querySelector("#loading");
+const loader = document.querySelector("#loader");
 
-let displayLoading = () => {
+let displayLoader = () => {
   loader.classList.add("display");
   setTimeout(() => {
     loader.classList.remove("display");
   }, 5000);
 }
 
-let hideLoading = () => {
+let hideLoader = () => {
   loader.classList.remove("display");
 }
 
 const showGallery = () => {
-  displayLoading()
+  displayLoader()
   fetch(
     API_URL
   )
@@ -23,16 +29,16 @@ const showGallery = () => {
       return response.json();
     })
     .then((data) => {
-      hideLoading()
+      hideLoader()
       data.photos.photo.map((gallery) => {
         const id = gallery.id;
         const serverId = gallery.server;
         const secret = gallery.secret;
-        container.innerHTML += `<img src="https://live.staticflickr.com/${serverId}/${id}_${secret}_w.jpg"/>`;
+        container.innerHTML += `<a href="https://live.staticflickr.com/${serverId}/${id}_${secret}_w.jpg"><img src="https://live.staticflickr.com/${serverId}/${id}_${secret}_w.jpg" alt="images of Olafur Eliasson"/></a>`;
       });
     })
     .catch((err) => {
-      document.getElementById("gallery").innerHTML = `<h1>Sorry</h1><p>We got nothing to show. Please try again!</p>`
+      document.getElementById("gallery").innerHTML = `<h1>Sorry</h1><p>We got nothing to show. Come back later!</p>`
       console.log("caught error", err);
     });
 
