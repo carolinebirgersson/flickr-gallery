aca323b43f01656254816dc3ed449c6dad0ca38e
caroline birgersson <caroline@birgerochblom.se>
2021-06-07 15:46:14 +0200

    changes to accordion and adding scroll to top button

--- code/script.js
+++ code/script.js
@@ -1,9 +1,13 @@
 // Accordion
-function toggle () {
-  this.classList.toggle("open")
-}
+const accordionButton = document.getElementById("accordion-button")
+const plusContainer = document.getElementById("plus")
+plusContainer.innerHTML += `<p>+</p>`;
 
-document.getElementById("accordion-button").onclick = toggle
+accordionButton.onclick = () => {
+  const infoContainer = accordionButton.nextElementSibling
+  infoContainer.classList.toggle("active")
+  plusContainer.classList.toggle("remove")
+}
 
 // Loader
 const loader = document.querySelector("#loader");
@@ -22,6 +26,7 @@ let hideLoader = () => {
 // Flickr Gallery
 const API_KEY = "449422936ab75f7bef9649f4cff24200";
 const API_URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=lecorbusier&format=json&nojsoncallback=1`
+const errorMessage = "No photos to show. Come back later!";
 
 const showGallery = () => {
   displayLoader()
@@ -37,11 +42,11 @@ const showGallery = () => {
         const id = gallery.id;
         const serverId = gallery.server;
         const secret = gallery.secret;
-        container.innerHTML += `<img src="https://live.staticflickr.com/${serverId}/${id}_${secret}_w.jpg" alt="images of Olafur Eliasson"/>`;
+        container.innerHTML += `<img src="https://live.staticflickr.com/${serverId}/${id}_${secret}_w.jpg" alt="images of Le Corbusier"/>`;
       });
     })
     .catch((err) => {
-      document.getElementById("gallery").innerHTML = `<h2>Sorry</h2><p>No photos to show. Come back later!</p>`
+      document.getElementById("gallery").innerHTML = `${errorMessage}`
       console.log("caught error", err);
     });
 
@@ -49,3 +54,23 @@ const showGallery = () => {
   }
 
   showGallery();
+
+  //Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+const scrollFunction = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+const topFunction = () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, Edge
+}
