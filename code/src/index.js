29eae3705cf6696a134f16e96098bacf6d86256d
caroline birgersson <caroline@birgerochblom.se>
2021-08-14 10:15:51 +0200

    cleaned classes/css

--- code/src/index.js
+++ code/src/index.js
@@ -1,37 +1,35 @@
 // Accordion
-const accordionButton = document.getElementById('accordion-container');
-const plusContainer = document.getElementById('expand-symbol');
-plusContainer.innerHTML += '+';
+const accordionButton = document.getElementById("accordion-container");
+const plusContainer = document.getElementById("expand-symbol");
+plusContainer.innerHTML += "+";
 accordionButton.onclick = () => {
   const infoContainer = accordionButton.nextElementSibling;
-  infoContainer.classList.toggle('active');
-  plusContainer.classList.toggle('remove');
+  infoContainer.classList.toggle("active");
+  plusContainer.classList.toggle("remove");
 };
 
 // Loader
-const loader = document.querySelector('#loader');
+const loader = document.querySelector("#loader");
 
 const displayLoader = () => {
-  loader.classList.add('display');
+  loader.classList.add("display");
 };
 
 const hideLoader = () => {
-  loader.classList.remove('display');
+  loader.classList.remove("display");
 };
 
 // Flickr Gallery
-const API_KEY = '449422936ab75f7bef9649f4cff24200';
+const API_KEY = "449422936ab75f7bef9649f4cff24200";
 const API_URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=lecorbusier&per_page=30&format=json&nojsoncallback=1`;
 
-const galleryContainer = document.getElementById('gallery');
-const errorMessage = 'Sorry, no photos to show. Come back later!';
+const galleryContainer = document.getElementById("gallery");
+const errorMessage = "Sorry, no photos to show. Come back later!";
 
 const showGallery = () => {
   displayLoader();
   fetch(API_URL)
-    .then((response) => {
-      return response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
       // eslint-disable-next-line array-callback-return
       data.photos.photo.map((gallery) => {
@@ -42,8 +40,9 @@ const showGallery = () => {
       });
     })
     .catch((error) => {
-      document.getElementById('gallery').innerHTML = errorMessage;
-      console.warn('caught error', error);
+      document.getElementById("gallery").innerHTML = errorMessage;
+      // eslint-disable-next-line no-console
+      console.warn("caught error", error);
     })
     .finally(() => {
       hideLoader();
@@ -53,13 +52,13 @@ const showGallery = () => {
 showGallery();
 
 // Scroll to top nav
-const scrollButton = document.getElementById('topScrollButton');
+const scrollButton = document.getElementById("topScrollButton");
 
 const showButton = () => {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    scrollButton.style.display = 'block';
+    scrollButton.style.display = "block";
   } else {
-    scrollButton.style.display = 'none';
+    scrollButton.style.display = "none";
   }
 };
 
