bde8bf19c69499e4dc428cb3c28ed554e952b5fc
caroline birgersson <caroline@birgerochblom.se>
2021-08-15 19:26:16 +0200

    fixed loader, smooth scroll, npm and lint

---
+++ code/public/index.js
@@ -0,0 +1,64 @@
+// Accordion
+const accordionButton = document.getElementById("accordion-container");
+const plusContainer = document.getElementById("expand-symbol");
+plusContainer.innerHTML += "+";
+accordionButton.onclick = () => {
+  const infoContainer = accordionButton.nextElementSibling;
+  infoContainer.classList.toggle("active");
+  plusContainer.classList.toggle("remove");
+};
+
+// Loader
+const loader = document.querySelector("#loader");
+
+const displayLoader = () => {
+  loader.classList.add("display");
+};
+
+const hideLoader = () => {
+  loader.classList.remove("display");
+};
+
+// Flickr Gallery
+const API_KEY = "449422936ab75f7bef9649f4cff24200";
+const API_URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=lecorbusier&per_page=30&format=json&nojsoncallback=1`;
+
+const galleryContainer = document.getElementById("gallery");
+const errorMessage = "Sorry, no photos to show. Come back later!";
+
+const showGallery = () => {
+  displayLoader();
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      // eslint-disable-next-line array-callback-return
+      data.photos.photo.map((gallery) => {
+        const { id } = gallery;
+        const { server } = gallery;
+        const { secret } = gallery;
+        galleryContainer.innerHTML += `<img src="https://live.staticflickr.com/${server}/${id}_${secret}_b.jpg" alt="image of Le Corbusier"/>`;
+      });
+    })
+    .catch((error) => {
+      document.getElementById("gallery").innerHTML = errorMessage;
+      // eslint-disable-next-line no-console
+      console.warn("caught error", error);
+    })
+    .finally(() => {
+      hideLoader();
+    });
+};
+
+showGallery();
+
+// Scroll to top nav
+const scrollButton = document.getElementById("topScrollButton");
+
+scrollButton.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
+
+window.onscroll = () => {
+  // eslint-disable-next-line no-unused-expressions
+  window.scrollY > 500
+    ? (scrollButton.style.display = "block")
+    : (scrollButton.style.display = "none");
+};
